dff34a7b33613bb70b53c7eec0bdde9786c94ac4
Federico Ceteroni <developer@overside.it>
2023-09-27 09:57:13 +0200

    Aggiungere i file di progetto.

---
+++ Classes/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester.Classes
+{
+    public class Calculator
+    {
+        public T Summ<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 + number2;
+        }
+        public T Difference<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 - number2;
+        }
+        public T Multiplier<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 * number2;
+        }
+        public T Division<T>(T number1, T number2) where T : INumber<T>
+        {
+            return number1 / number2;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace net_calculator_tester
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
